6b5702b1afb9c5a2ed167c26833b84be84d62b7c
Jonathan <jonacom@lissismore.com>
2016-03-11 10:37:20 -0700

    I3: guessing game works wooo

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,8 +3,24 @@ require 'sinatra/reloader'
 
 set :port, 9292
 
-number = rand(100)
-
+number = rand(100) + 1
 get '/' do
-  erb :index, :locals => {:number => number}
+  message = check_guess(params["guess"].to_i, number)
+  erb :index, :locals => {:number => number, :message => message}
+end
+
+def check_guess(guess, number)
+  if guess <= 0
+    "Guess a number between 1-100"
+  elsif guess == number
+    "You win!"
+  elsif number - guess <= -5
+    "Way too high"
+  elsif guess > number
+    "Too high"
+  elsif number - guess >= 5
+    "Way too low"
+  else
+    "Too low"
+  end
 end
